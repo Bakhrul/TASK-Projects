fbbb511d7a8891b45e47594559bba09750c5ea0a
Bakhrul <Bakhrulrpl@gmail.com>
2020-11-11 15:36:12 +0700

    push

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,12 @@
-function showPassword(evt) {
+function showHidePassword(evt) {
+    
     var input = document.getElementById("input-password");
-    input.type = "text";
+    if(input.type == 'text'){
+        input.type = "password";
+        evt.style.color = "#bfbfbf";
+    }else{
+        input.type = "text";
     evt.style.color = "#AA084B";
-}
-
-function hidePassword(evt) {
-    var input = document.getElementById("input-password");
-    input.type = "password";
-    evt.style.color = "#bfbfbf";
+    }
+    
 }
