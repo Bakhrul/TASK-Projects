cd356d386ca50fcbca71793bd3147a889ebf3d00
Bakhrul <Bakhrulrpl@gmail.com>
2020-11-12 11:14:24 +0700

    push

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,17 @@
+var child = document.querySelector(".navigation-bottom-mobile");
+var parent = document.querySelector(".footer-copy-with-navigation-bottom");
+
+var childHeight = parseInt(window.getComputedStyle(child).height) + "px";
+parent.style.marginBottom = childHeight;
+
 function showHidePassword(evt) {
-    
+
     var input = document.getElementById("input-password");
-    if(input.type == 'text'){
+    if (input.type == 'text') {
         input.type = "password";
         evt.style.color = "#bfbfbf";
-    }else{
+    } else {
         input.type = "text";
-    evt.style.color = "#AA084B";
+        evt.style.color = "#AA084B";
     }
-    
 }
